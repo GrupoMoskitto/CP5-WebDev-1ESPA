4187dd4a44feadfb41d9560826a6b6d625aeef55
Kato <kato8088@users.noreply.github.com>
2025-05-10 15:47:38 -0300

    Estilizando o Header

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -1,15 +1,19 @@
 import React from 'react';
+import logoMoskitto from '../src/assets/logoMoskitto_oficial.jpeg';
 
 const Header = () => {
   return (
-    <header className="bg-blue-500 text-white p-4">
+    <header className="bg-green-800 text-white p-4">
       <div className="container mx-auto flex justify-between items-center">
-        <h1 className="text-2xl font-bold">Meu Site</h1>
+        <div style={{width:'7vh'}}>
+          <img src={logoMoskitto} />
+        </div>
+        <h1 className="text-2xl font-sans font-bold ml-30">Loja de Bicicletas Elétricas Moskitto</h1>
         <nav>
           <ul className="flex space-x-4">
-            <li><a href="#home" className="hover:underline">Home</a></li>
-            <li><a href="#about" className="hover:underline">Sobre</a></li>
-            <li><a href="#contact" className="hover:underline">Contato</a></li>
+            <li><a href="/" className="hover:text-black transition duration-300">Home</a></li>
+            <li><a href="#" className="hover:text-black transition duration-300">Sobre</a></li>
+            <li><a href="/contato" className="hover:text-black transition duration-300">Contato</a></li>
           </ul>
         </nav>
       </div>
