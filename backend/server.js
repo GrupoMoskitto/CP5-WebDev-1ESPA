c69c969c7ae658ddced64bc84e1f68c2836a2a1b
Kato <kato8088@users.noreply.github.com>
2025-05-10 13:09:25 -0300

    Implementando Banco de Dados SQLite3

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,65 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
+const db = require('./database'); // Importar o banco de dados
 const app = express();
 const PORT = 5000;
 
 app.use(bodyParser.json());
 app.use(cors());
 
-let bicicletas = [];
-
 // criar um card de produto (bicicleta)
 app.post('/bicicletas', (req, res) => {
-  const bicicleta = req.body;
-  bicicletas.push(bicicleta);
-  res.status(201).send(bicicleta);
+  const { id, name, description, price } = req.body;
+  db.run("INSERT INTO bicicletas (id, name, description, price) VALUES (?, ?, ?, ?)", [id, name, description, price], function(err) {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.status(201).send(req.body);
+  });
 });
 
 // requisição de todos os cards
 app.get('/bicicletas', (req, res) => {
-  res.send(bicicletas);
+  db.all("SELECT * FROM bicicletas", [], (err, rows) => {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.send(rows);
+  });
 });
 
 // editar um card/produto
 app.put('/bicicletas/:id', (req, res) => {
   const { id } = req.params;
-  const updatedbicicleta = req.body;
-  bicicletas = bicicletas.map(bicicleta => bicicleta.id === id ? updatedbicicleta : bicicleta);
-  res.send(updatedbicicleta);
+  const { name, description, price } = req.body;
+  db.run("UPDATE bicicletas SET name = ?, description = ?, price = ? WHERE id = ?", [name, description, price, id], function(err) {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.send(req.body);
+  });
 });
 
 // deletar um card/produto
 app.delete('/bicicletas/:id', (req, res) => {
   const { id } = req.params;
-  bicicletas = bicicletas.filter(bicicleta => bicicleta.id !== id);
-  res.status(204).send();
+  db.run("DELETE FROM bicicletas WHERE id = ?", [id], function(err) {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.status(204).send();
+  });
+});
+
+// deletar todos os produtos
+app.delete('/bicicletas', (req, res) => {
+  db.run("DELETE FROM bicicletas", function(err) {
+    if (err) {
+      return res.status(500).send(err.message);
+    }
+    res.status(204).send();
+  });
 });
 
 // debug: confirmacao da porta do servidor no console
