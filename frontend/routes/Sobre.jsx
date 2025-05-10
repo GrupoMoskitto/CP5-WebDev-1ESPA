26ef3b6ae99932a62c95d719a6ce7e284579a5d9
Kato <kato8088@users.noreply.github.com>
2025-05-10 17:57:40 -0300

    Criando a página sobre

---
+++ frontend/routes/Sobre.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Sobre = () => {
+  return (
+    <div className='flex flex-col items-center mt-10'>
+      <h1 className='text-3xl mb-10 text-green-700 text-shadow-lg'>Conheça mais sobre a Loja de Bicicletas Elétricas Moskitto:</h1>
+      <div className='w-full max-w-2xl p-6 shadow-xl/30 rounded-lg text-green-800 bg-green-100'>
+        <p className='text-lg'>
+             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas placerat luctus augue luctus ultrices. Fusce rhoncus quam a velit tempor placerat et vitae tortor. Nam porta tristique elit. Suspendisse ultricies eros ut elit ultricies mattis. Aliquam maximus leo sed nisl semper vehicula. Suspendisse potenti. Ut ut convallis nisi.
+             Praesent et sem sed sem cursus ultricies sit amet in quam. Mauris eget nibh ut urna pharetra molestie. Mauris in massa ex. Cras faucibus venenatis eros ut posuere. Nullam volutpat ligula sed rutrum sagittis. Vestibulum ultricies rhoncus congue. Phasellus sed nunc vel felis porta tempus. Etiam eleifend commodo ex. Nunc pulvinar porttitor eros, ac semper sem hendrerit ullamcorper. Vivamus at dolor vitae augue facilisis pharetra. Nullam finibus, leo sit amet mollis sodales, augue neque laoreet justo, in feugiat est dolor a nisi. Suspendisse urna lectus, maximus ac accumsan a, tempus eget nulla.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default Sobre
