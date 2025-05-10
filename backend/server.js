0c952cdead5de19c172b7b41587c926973b0632c
Kato <kato8088@users.noreply.github.com>
2025-05-10 12:03:36 -0300

    Criando o servidor (backend)

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,43 @@
-const express =require("express")
-const cors =require("cors")
-const bodyParser =require("body-parser")
-const {v4:uuid} =require("uuid")
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 
-const app =express();
+const app = express();
+const PORT = 5000;
 
-const Port=3000;
-
-app.use(cors());
 app.use(bodyParser.json());
+app.use(cors());
 
-let produtos=[];
+let bicicletas = [];
+
+// criar um card de produto (bicicleta)
+app.post('/bicicletas', (req, res) => {
+  const bicicleta = req.body;
+  bicicletas.push(bicicleta);
+  res.status(201).send(bicicleta);
+});
+
+// requisição de todos os cards
+app.get('/bicicletas', (req, res) => {
+  res.send(bicicletas);
+});
+
+// editar um card/produto
+app.put('/bicicletas/:id', (req, res) => {
+  const { id } = req.params;
+  const updatedbicicleta = req.body;
+  bicicletas = bicicletas.map(bicicleta => bicicleta.id === id ? updatedbicicleta : bicicleta);
+  res.send(updatedbicicleta);
+});
+
+// deletar um card/produto
+app.delete('/bicicletas/:id', (req, res) => {
+  const { id } = req.params;
+  bicicletas = bicicletas.filter(bicicleta => bicicleta.id !== id);
+  res.status(204).send();
+});
+
+// confirmacao da porta do servidor no console
+app.listen(PORT, () => {
+  console.log(`servidor rodando na porta ${PORT}.`);
+});
