1dcbb709082ac1aead7663135019c54c5384411f
Kato <kato8088@users.noreply.github.com>
2025-05-10 18:50:38 -0300

    Adicionando imagens aos Cards de Produto

--- frontend/components/CardProduto.jsx
+++ frontend/components/CardProduto.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 
 const CardProduto = ({ bicicleta, onEdit, onDelete }) => {
+  const listaImagens = [
+    '../src/assets/bike1.jpg',
+    '../src/assets/bike2.jpg',
+    '../src/assets/bike3.jpg',
+    '../src/assets/bike4.jpg',
+    '../src/assets/bike5.jpg',
+    '../src/assets/bike6.jpg',
+    '../src/assets/bike7.jpg',
+    '../src/assets/bike8.jpg',
+    '../src/assets/bike9.jpg',
+    '../src/assets/bike10.jpg',
+    '../src/assets/bike11.jpg',
+    '../src/assets/bike12.jpg'
+  ];
+
+  const getImagemAleatoria = () => {
+    const indexRandom = Math.floor(Math.random() * listaImagens.length);
+    return listaImagens[indexRandom];
+  };
+
+  const imagemAleatoria = getImagemAleatoria();
+
   return (
-    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-sm">
-      <img className="p-8 rounded-t-lg" src={bicicleta.image} alt={bicicleta.name} />
+    <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow-lg/40">
+      <img className="p-8 rounded-t-lg" src={imagemAleatoria} alt={bicicleta.name} />
       <div className="px-5 pb-5">
         <h5 className="text-xl font-semibold tracking-tight text-gray-900">{bicicleta.name}</h5>
         <div className="flex items-center justify-between">
